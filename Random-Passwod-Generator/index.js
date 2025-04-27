a10f990d4031109eec547581a1637a14f71733a9
ULLAS S A <ullas200410@gmail.com>
2025-04-27 06:04:04 +0530

    Update index.js

--- Random-Passwod-Generator/index.js
+++ Random-Passwod-Generator/index.js
@@ -1,1 +1,45 @@
 
+function generatePassword(length, includeLowercase, includeUpperrcase, includeNumbers, includeSymbols) {
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numberChars = "0123456789";
+    const symbolChars = "!@#$%^&*()_+";
+
+    let allowedChars = "";
+    let password = "";
+
+    allowedChars += includeLowercase ? lowercaseChars : "";
+    allowedChars += includeUpperrcase ? uppercaseChars : "";
+    allowedChars += includeNumbers ? numberChars : "";
+    allowedChars += includeSymbols ? symbolChars : "";
+
+    console.log("Allowed characters:", allowedChars);
+
+    if (allowedChars.length === 0) {
+        return "Please select at least one character type!";
+    }
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * allowedChars.length);
+        password += allowedChars[randomIndex];
+    }
+
+    return password;
+}
+
+
+const passwordlength = 12;
+const includeLowercase = true;
+const includeUpperrcase = true;
+const includeNumbers = true;
+const includeSymbols = true;
+
+const password = generatePassword(
+    passwordlength,
+    includeLowercase,
+    includeUpperrcase,
+    includeNumbers,
+    includeSymbols
+);
+
+console.log(`Generated password: ${password}`);
